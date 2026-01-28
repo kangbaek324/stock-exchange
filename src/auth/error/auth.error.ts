9a8e5532c614007ce3cc4cb3b40b8097a060c206
kangbaekho <kbh081004@gmail.com>
2026-01-28 18:37:14 +0900

    refactor: 회원가입시 에러 커스텀 에러로 변경

--- src/auth/error/auth.error.ts
+++ src/auth/error/auth.error.ts
@@ -1,10 +1,15 @@
 import { HttpStatus } from "@nestjs/common";
 
 export const AuthError = {
-    AUTH: {
+    ALREADY_EXIST_NAME: {
         code: "AUTH_001",
-        status: HttpStatus.NOT_FOUND,
-        message: "테스트 입니다."
+        status: HttpStatus.CONFLICT,
+        message: "이미 사용중인 이름입니다."
+    },
+    ALREADY_EXIST_EMAIL: {
+        code: "AUTH_002",
+        status: HttpStatus.CONFLICT,
+        message: "이미 사용중인 이메일입니다."
     }
 } as const;
 
