da5797b029d8d351a3b314f9d7c65d6575f6111f
kangbaek324 <kbh081004@gmail.com>
2026-08-11 11:12:49 +0900

    feat: 주식 리스트 반환 API 현재 등락률 필드 추가

--- src/modules/stock/stock.module.ts
+++ src/modules/stock/stock.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { StockService } from './stock.service';
 import { StockController } from './stock.controller';
 import { StockPublishRelay } from './stock-publish.relay';
+import { OrderModule } from '../order/order.module';
 
 @Module({
+    imports: [OrderModule],
     controllers: [StockController],
     providers: [StockService, StockPublishRelay],
 })
