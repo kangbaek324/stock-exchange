5e5e562fddb84eded51a8504b2c13be22725f4d3
BaekHo Kang <kbh081004@gmail.com>
2026-09-09 23:53:14 +0900

    chore: MAX_STOCK limit 상향

--- src/common/consants/account.constants.ts
+++ src/common/consants/account.constants.ts
@@ -1,4 +1,4 @@
 export const ACCOUNT_AMOUNT_LIMIT = {
     MAX_BALANCE: 1_000_000_000_000, // 잔고 1회 입·출금/송금 한도 (1조)
-    MAX_STOCK: 10_000_000, // 보유 주식 1회 입·출고 한도 (1천만 주)
+    MAX_STOCK: 100_000_000_000,     // 보유 주식 1회 입·출고 한도 (1천억 주)
 } as const;
